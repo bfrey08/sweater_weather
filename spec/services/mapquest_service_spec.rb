237adf7182577545a5725cfbeac320b53f7548f3
Billy Frey <bfrey16@gmail.com>
2022-01-16 19:39:09 -0700

    test: mapquest service spec complete

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe MapquestService do
+  it 'can return the lat/long' do
+    location = "Denver,CO"
 
+    response = MapquestService.forecast(location)
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:results)
+    expect(response[:results]).to have_key(:latLng)
+    expect(response[:results][:latLng]).to have_key(:lat)
+    expect(response[:results][:latLng]).to have_key(:lng)
+
+  end
 
 end
