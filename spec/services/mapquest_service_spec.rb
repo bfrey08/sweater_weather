8cd60d35252868b35d3a5ba2ab4e2367c3b81eb1
Billy Frey <bfrey16@gmail.com>
2022-01-16 20:38:34 -0700

    complete openweather and mapquest services w/ passing tests and vcr

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe MapquestService do
   it 'can return the lat/long' do
+    VCR.use_cassette("mapquest_denver,CO") do
     location = "Denver,CO"
 
-    response = MapquestService.forecast(location)
+    response = MapquestService.coordinates(location)
 
     expect(response).to be_a(Hash)
     expect(response).to have_key(:results)
-    expect(response[:results]).to have_key(:latLng)
-    expect(response[:results][:latLng]).to have_key(:lat)
-    expect(response[:results][:latLng]).to have_key(:lng)
-
+    expect(response[:results][0]).to have_key(:locations)
+    expect(response[:results][0][:locations][0]).to have_key(:latLng)
+    expect(response[:results][0][:locations][0][:latLng]).to have_key(:lat)
+    expect(response[:results][0][:locations][0][:latLng]).to have_key(:lng)
+    end
   end
 
 end
--- spec/services/openweather_service_spec.rb
+++ spec/services/openweather_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe OpenweatherService do
   describe 'get forecast' do
     it 'can receive a forecast based on lattitude and longitude of a city' do
+      VCR.use_cassette("openweather_denver,CO") do
 
       denver_lat = 39.74
       denver_lon = 105.00
@@ -21,28 +22,30 @@ describe OpenweatherService do
       expect(response[:current]).to have_key(:sunset)
       expect(response[:current]).to have_key(:temp)
       expect(response[:current]).to have_key(:feels_like)
-      expect(response[:current]).to have_key(:hummidity)
+      expect(response[:current]).to have_key(:humidity)
       expect(response[:current]).to have_key(:uvi)
       expect(response[:current]).to have_key(:visibility)
       expect(response[:current]).to have_key(:weather)
-      expect(response[:current][:weather]).to have_key(:description)
-      expect(response[:current][:weather]).to have_key(:icon)
+      expect(response[:current][:weather][0]).to have_key(:description)
+      expect(response[:current][:weather][0]).to have_key(:icon)
 
-      expect(response[:daily]).to be_a(Hash)
-      expect(response[:daily]).dailyto have_key(:dt)
-      expect(response[:daily]).to have_key(:sunrise)
-      expect(response[:daily]).to have_key(:sunset)
-      expect(response[:daily]).to have_key(:temp)
-      expect(response[:daily][:temp]).to have_key(:max)
-      expect(response[:daily][:temp]).to have_key(:min)
-      expect(response[:daily][:weather]).to have_key(:description)
-      expect(response[:daily][:weather]).to have_key(:icon)
+      expect(response[:daily][0]).to be_a(Hash)
+      expect(response[:daily][0]).to have_key(:dt)
+      expect(response[:daily][0]).to have_key(:sunrise)
+      expect(response[:daily][0]).to have_key(:sunset)
+      expect(response[:daily][0]).to have_key(:temp)
+      expect(response[:daily][0][:temp]).to have_key(:max)
+      expect(response[:daily][0][:temp]).to have_key(:min)
+      expect(response[:daily][0][:weather][0]).to have_key(:description)
+      expect(response[:daily][0][:weather][0]).to have_key(:icon)
 
 
-      expect(response[:hourly]).to have_key(:dt)
-      expect(response[:hourly]).to have_key(:temp)
-      expect(response[:hourly]).to have_key(:weather)
-      expect(response[:hourly][:weather]).to have_key(:description)
-      expect(response[:hourly][:weather]).to have_key(:icon)
+      expect(response[:hourly][0]).to have_key(:dt)
+      expect(response[:hourly][0]).to have_key(:temp)
+      expect(response[:hourly][0]).to have_key(:weather)
+      expect(response[:hourly][0][:weather][0]).to have_key(:description)
+      expect(response[:hourly][0][:weather][0]).to have_key(:icon)
+      end
     end
+  end
 end
