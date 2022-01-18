1d61947834ed21154000d556c74cdb9b4396d208
Billy Frey <bfrey16@gmail.com>
2022-01-18 10:29:55 -0700

    fix: facade tests

--- spec/facades/openlibrary_facade_spec.rb
+++ spec/facades/openlibrary_facade_spec.rb
@@ -6,19 +6,19 @@ require 'rails_helper'
       @quantity = 5
     end
     it 'can create an array of books', :vcr do
-      response = OpenlibraryFacade.location_search(@location)
+      response = OpenlibraryFacade.books(@location)
 
       expect(response).to be_an(Array)
       expect(response[0]).to be_an_instance_of(Book)
     end
     it 'can get number of books based on location search', :vcr do
-      response = OpenlibraryFacade.location_search(@location)
+      response = OpenlibraryFacade.quantity(@location)
 
       expect(response).to be_an(Integer)
       expect(response).not_to eq(0)
     end
     it 'can create a book_search object', :vcr do
-      response = OpenlibraryFacade.location_search(@location, @quantity)
+      response = OpenlibraryFacade.book_search(@location, @quantity)
 
       expect(response).to be_an_instance_of(BookSearch)
       expect(response.books).to be_an(Array)
