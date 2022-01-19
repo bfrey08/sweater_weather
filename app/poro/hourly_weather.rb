9813b07c7b812342464eb3aac19824ee92a120fd
Billy Frey <bfrey16@gmail.com>
2022-01-19 03:45:19 -0700

    refactor: use inheratence in hourly_weather

--- app/poro/hourly_weather.rb
+++ app/poro/hourly_weather.rb
@@ -1,4 +1,4 @@
-class HourlyWeather
+class HourlyWeather < CurrentWeather
   attr_reader :time,
               :temperature,
               :conditions,
@@ -10,10 +10,5 @@ class HourlyWeather
     @conditions = forecast[:weather][0][:description]
     @icon = forecast[:weather][0][:icon]
   end
-
-  def temperature_f
-    ((@temperature - 273.15) * (1.8) + 32).round(0).to_s.concat(" F")
-  end
-
 
 end
